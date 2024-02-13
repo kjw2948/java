d1aed6b220532a7e757d3cbcff7a44b1aa4eef58
kjw2948 <kjw970313@cau.ac.kr>
2024-02-13 12:48:50 +0900

    excercise2

--- src/excercise/java/Excercise2.java
+++ src/excercise/java/Excercise2.java
@@ -1,7 +1,20 @@
 package excercise.java;
-
+import java.util.Arrays;
 public class Excercise2 {
+    public static void solution(int[] num){
+        int idx = 0;
+        for (int i : num) {
+            if(idx==0 || i>num[idx-1]){
+                num[idx++] = i;
+            }
+        }
+        System.out.print("["+(idx) +"] ");
+        for (int i = 0; i < idx; i++) {
+            System.out.print(num[i]+ "\t");
+        }
+    }
     public static void main(String[] args) {
-
+        int[] arr1 = {0,0,1,1,2};
+        solution(arr1);
     }
 }
