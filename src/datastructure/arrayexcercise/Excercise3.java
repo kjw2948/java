bb7bd53e85f105803f93464d1dac97003a163ec7
kjw2948 <kjw970313@cau.ac.kr>
2024-02-14 14:06:04 +0900

    array excercise3

---
+++ src/datastructure/arrayexcercise/Excercise3.java
@@ -0,0 +1,30 @@
+package datastructure.arrayexcercise;
+
+public class Excercise3 {
+    public static void main(String[] args) {
+        int index = 0;
+        int[] arr = {3,1,2,6,2,2,5,1,9,10,1,11};
+        int[] sol = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if(i==0){
+                if(arr[i]>arr[i+1]){
+                    sol[index++] = arr[i];
+                    i++;
+                }
+            }
+            else if(i==arr.length-1){
+                if(arr[i]>arr[i-1]){
+                    sol[index++] = arr[i];
+                }
+            }
+            else{
+                if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+                    sol[index++] = arr[i];
+                }
+            }
+        }
+        for (int i = 0; i < index; i++) {
+            System.out.print(sol[i] + "\t");
+        }
+    }
+}
