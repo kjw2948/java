8a5084d9600d12636615908035968bbbf2a9cd61
kjw2948 <kjw970313@cau.ac.kr>
2024-02-14 13:48:00 +0900

    array excercise2

---
+++ src/datastructure/arrayexcercise/Excercise1.java
@@ -0,0 +1,21 @@
+package datastructure.arrayexcercise;
+
+public class Excercise1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 100, 1, 1 ,1 ,100};
+        int index = -1;
+        int target = 1;
+        for (int i = arr.length-1; i > 0; i--) {
+            if(arr[i] == target){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            System.out.println("해당 target이 없습니다");
+        }
+        else{
+            System.out.println(index);
+        }
+    }
+}
---
+++ src/datastructure/arrayexcercise/Excercise2.java
@@ -0,0 +1,14 @@
+package datastructure.arrayexcercise;
+import java.util.Arrays;
+
+public class Excercise2 {
+        public static void main(String[] args) {
+            int[] arr = {1, 3, 7, 9};
+            for (int i = 0; i < arr.length/2; i++) {
+                int temp = arr[i];
+                arr[i] = arr[arr.length-1-i];
+                arr[arr.length-1-i] = temp;
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+}
