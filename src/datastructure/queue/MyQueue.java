837c72fa47e8740a281a27e57b73b4d474c76dfc
kjw2948 <kjw970313@cau.ac.kr>
2024-02-15 21:43:52 +0900

    Queue

---
+++ src/datastructure/queue/MyQueue.java
@@ -0,0 +1,27 @@
+package datastructure.queue;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class MyQueue {
+
+    public static void main(String[] args) {
+        Queue queue = new LinkedList();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        System.out.println(queue);
+
+        System.out.println(queue.poll());
+        System.out.println(queue);
+        System.out.println(queue.poll());
+        System.out.println(queue.peek());
+        System.out.println(queue.contains(3));
+        System.out.println(queue.size());
+        System.out.println(queue.isEmpty());
+
+    }
+
+}
