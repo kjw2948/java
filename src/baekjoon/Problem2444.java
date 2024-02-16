fc1849f90085a72cf3012cdf6c94fdc808631ece
kjw2948 <kjw970313@cau.ac.kr>
2024-02-16 22:40:43 +0900

    백준 2444번

---
+++ src/baekjoon/Problem2444.java
@@ -0,0 +1,29 @@
+package baekjoon;
+import java.util.Scanner;
+public class Problem2444 {
+    public static void solution(int N) {
+        for (int i = 0; i < N; i++) {
+            for (int j = N-1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j<2*i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int i = N-1; i > 0; i--) {
+            for (int j = i; j < N; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i*2-1; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        solution(sc.nextInt());
+    }
+}
