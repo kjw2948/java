101b59f51c801e5822e80abfe944ef2b1f084965
kjw2948 <kjw970313@cau.ac.kr>
2024-02-16 23:05:05 +0900

    백준 10988번

--- src/baekjoon/bronze/Problem10988.java
+++ src/baekjoon/bronze/Problem10988.java
@@ -1,4 +1,31 @@
 package baekjoon.bronze;
-
+import java.util.Scanner;
 public class Problem10988 {
+    public static int solution(String str){
+        boolean result = true;
+        for (int i = 0; i <str.length()/2 ; i++) {
+            if(str.charAt(i)==str.charAt(str.length()-1-i)) {result = true;}
+            else{
+                result = false;
+                break;
+            }
+        }
+        return (result == true) ? 1 : 0;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(solution(sc.next()));
+    }
 }
+
+
+/*
+import java.util.Scanner;
+public class Problem10988 {
+    public static boolean solution(String str){
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}*/
