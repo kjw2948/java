e88bdc98ca614fce775f796ed39484dbbfafa60c
kjw2948 <kjw970313@cau.ac.kr>
2024-02-20 12:37:40 +0900

    삽입 정렬

---
+++ src/algorithm/sort/InsertSort.java
@@ -0,0 +1,23 @@
+package algorithm.sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/* 삽입정렬 : 앞의 데이터를 정렬 해가면서 삽입 위치를 찾아 정렬하는 방식
+   복잡도 : O(n^2)
+ */
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 7, 1, 6};
+        for (int i = 1; i < arr.length-1; i++) {
+            for (int j = i; j >= 0 ; j--) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
