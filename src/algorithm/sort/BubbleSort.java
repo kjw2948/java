c4cc93fe4f919f78ff8ec0e2746943ac85d71865
kjw2948 <kjw970313@cau.ac.kr>
2024-02-20 12:31:24 +0900

    버블정렬

--- src/algorithm/sort/BubbleSort.java
+++ src/algorithm/sort/BubbleSort.java
@@ -16,7 +16,9 @@ public class BubbleSort {
         for (int i = 1; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i; j++) {
                 if(arr[j]>arr[j+1]){
-                    swap(arr[j], arr[j + 1]);
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
