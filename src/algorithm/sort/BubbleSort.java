0de9415c30b03ac66f6b1253a87d728a8a40cb1c
kjw2948 <kjw970313@cau.ac.kr>
2024-02-20 12:28:52 +0900

    버블정렬

---
+++ src/algorithm/sort/BubbleSort.java
@@ -0,0 +1,25 @@
+package algorithm.sort;
+
+import java.util.Arrays;
+
+/* 버블정렬 : 인접한 데이터를 비교하며 자리 바꾸는 방식
+   시간 복잡도 : O(n^2)
+ */
+public class BubbleSort {
+    public static void swap(int x, int y){
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 1, 7, 6};
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if(arr[j]>arr[j+1]){
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
