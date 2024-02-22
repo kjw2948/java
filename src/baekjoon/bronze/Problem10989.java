c4d28e5be951957ae1affb562ecc73475b8515cd
kjw2948 <kjw970313@cau.ac.kr>
2024-02-22 23:23:50 +0900

    백준 10989번 - 계수정렬

--- src/baekjoon/bronze/Problem10989.java
+++ src/baekjoon/bronze/Problem10989.java
@@ -1,33 +1,27 @@
 package baekjoon.bronze;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 //계수정렬
 public class Problem10989 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[N];
-        int[] result = new int[N];
-        int[] num_arr = new int[10001];
-        // step1. arr배열에 나타난 값의 개수 세기
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(bf.readLine());
-            num_arr[arr[i]]++;
-        }
-        // step2. num_arr 배열 누적합으로 바꾸기
-        for (int i = 1; i < num_arr.length; i++) {
-            num_arr[i] += num_arr[i - 1];
-        }
-        // step3. result 구하기
-        for (int i = arr.length-1; i >= 0; i--) {
-            int index = --num_arr[arr[i]];
-            result[index] = arr[i];
-        }
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+            int N = Integer.parseInt(br.readLine());
+
+            int count[] = new int[10001];
+            for(int i=0; i<N; i++) {
+                count[Integer.parseInt(br.readLine())]++;
+            }
+
+            for(int i=1; i<10001; i++) {
+                while(count[i] > 0) {
+                    sb.append(i).append('\n');
+                    count[i]--;
+                }
+            }
+
+            System.out.println(sb);
         }
-    }
 }
 
