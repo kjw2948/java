172ba849a1ae028e55017f38bf8ea86284f6e789
kjw2948 <kjw970313@cau.ac.kr>
2024-02-22 23:17:34 +0900

    백준 10989번 - 계수정렬

--- src/baekjoon/bronze/Problem10989.java
+++ src/baekjoon/bronze/Problem10989.java
@@ -1,17 +1,33 @@
 package baekjoon.bronze;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//계수정렬
 public class Problem10989 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int[] arr = new int[num];
-        for (int i = 0; i < num; i++) {
-            arr[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int[] arr = new int[N];
+        int[] result = new int[N];
+        int[] num_arr = new int[10001];
+        // step1. arr배열에 나타난 값의 개수 세기
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+            num_arr[arr[i]]++;
         }
-        Arrays.sort(arr);
-        for (int i = 0; i < num; i++) {
-            System.out.println(arr[i]);
+        // step2. num_arr 배열 누적합으로 바꾸기
+        for (int i = 1; i < num_arr.length; i++) {
+            num_arr[i] += num_arr[i - 1];
+        }
+        // step3. result 구하기
+        for (int i = arr.length-1; i >= 0; i--) {
+            int index = --num_arr[arr[i]];
+            result[index] = arr[i];
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
         }
     }
 }
+
