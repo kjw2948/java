240c28d9c9da8d1b18a183dc957d276b5984f1c5
kjw2948 <kjw970313@cau.ac.kr>
2024-03-01 21:56:50 +0900

    백준 2830

--- src/baekjoon/gold/Problem2830.java
+++ src/baekjoon/gold/Problem2830.java
@@ -8,13 +8,32 @@ import java.util.ArrayList;
 public class Problem2830 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int answer = 0;
-        int count = 0;
+        long answer = 0;
+        int idx = 0;
         int N = Integer.parseInt(bf.readLine());
-        ArrayList list = new ArrayList();
+        int[] saveOne = new int[20]; // 1갯수 저장
+        ArrayList<Integer> list = new ArrayList();
         for (int i = 0; i < N; i++) {
-            list.add(Integer.toBinaryString(Integer.parseInt(bf.readLine())));
+            list.add(Integer.parseInt(bf.readLine()));
         }
-
+        // 1갯수 저장
+        while(true) {
+            int sum = 0;
+            for (int i=0; i<list.size(); i++) {
+                if (list.get(i) % 2 == 1) {
+                    saveOne[idx] ++;
+                }
+                sum += list.get(i);
+                list.set(i, list.get(i) / 2);
+            }
+            if(sum==0){
+                break;
+            }
+            idx++;
+        }
+        for (int i = 0; i < 20; i++) {
+            answer += (long) saveOne[i]*(N-saveOne[i])<<i;
+        }
+        System.out.println(answer);
     }
 }
