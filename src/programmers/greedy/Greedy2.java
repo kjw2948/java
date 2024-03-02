ad2ac78da9f75f657a11e830a85c057bde2fbc22
kjw2948 <kjw970313@cau.ac.kr>
2024-03-02 21:51:02 +0900

    그리디 알고리즘

---
+++ src/programmers/greedy/Greedy2.java
@@ -0,0 +1,28 @@
+package programmers.greedy;
+
+class Joystick{
+    int count;
+    int idx;
+    public void moveUp() {
+        if()
+        this.idx ++;
+        this.count ++;
+    }
+
+    public void movdDown() {
+
+    }
+
+    public void moveLeft() {
+
+    }
+
+    public void moveRigth() {
+
+    }
+}
+public class Greedy2 {
+    public static void main(String[] args) {
+
+    }
+}
