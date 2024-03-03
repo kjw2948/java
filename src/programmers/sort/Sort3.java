9505b61515622df87f1c0633ee6fc2fb6967a49c
kjw2948 <kjw970313@cau.ac.kr>
2024-03-03 19:20:00 +0900

    정렬 문제3

---
+++ src/programmers/sort/Sort3.java
@@ -0,0 +1,17 @@
+package programmers.sort;
+
+import java.util.Arrays;
+
+public class Sort3 {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        int idx = citations.length / 2 ;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+    }
+}
