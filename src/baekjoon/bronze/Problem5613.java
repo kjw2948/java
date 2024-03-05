e31549f3226f75ed87b82d180f88bcc6fda705e3
kjw2948 <kjw970313@cau.ac.kr>
2024-03-05 13:09:16 +0900

    백준 5613번

--- src/baekjoon/bronze/Problem5613.java
+++ src/baekjoon/bronze/Problem5613.java
@@ -1,4 +1,36 @@
 package baekjoon.bronze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Problem5613 {
+    public static int solution() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int answer = Integer.parseInt(bf.readLine());
+        while(true){
+            String str = bf.readLine();
+            if (str.equals("=")) {
+                break;
+            }
+            switch(str){
+                case "+" :
+                    answer += Integer.parseInt(bf.readLine());
+                    break;
+                case "-" :
+                    answer -= Integer.parseInt(bf.readLine());
+                    break;
+                case "*" :
+                    answer *= Integer.parseInt(bf.readLine());
+                    break;
+                case "/" :
+                    answer /= Integer.parseInt(bf.readLine());
+                    break;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException{
+        System.out.println(solution());
+    }
 }
