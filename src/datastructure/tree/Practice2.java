62d3afcbac2340b35ae27c1fdad4035181e3f905
kjw2948 <kjw970313@cau.ac.kr>
2024-03-07 20:13:16 +0900

    이진트리 순회 연결리스트 구현

---
+++ src/datastructure/tree/Practice2.java
@@ -0,0 +1,20 @@
+package datastructure.tree;
+
+class Node{
+    char data;
+    Node left;
+    Node right;
+
+    public Node(char data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinaryTree2 {
+    Node head;
+}
+public class Practice2 {
+
+}
