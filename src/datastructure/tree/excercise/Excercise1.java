453306c36c9c3ae1e9c587c7084666bb6b001ed0
kjw2948 <kjw970313@cau.ac.kr>
2024-03-07 20:40:14 +0900

    책 펴기

---
+++ src/datastructure/tree/excercise/Excercise1.java
@@ -0,0 +1,30 @@
+package datastructure.tree.excercise;
+
+import java.util.TreeMap;
+
+public class Excercise1 {
+    public static void inOrder(int[] arr, int idx) {
+        int left = idx * 2 + 1;
+        int right = idx * 2 + 2;
+        if(left<arr.length -1){
+            inOrder(arr, left);
+        }
+        System.out.print(arr[idx]+", ");
+        if(right<arr.length -1){
+            inOrder(arr, right);
+        }
+    }
+    public static void solution(int n) {
+        int[] tree = new int[(int)Math.pow(2, n)];
+        tree[0] = 0;
+        for (int i = 0; i < Math.pow(2, (n-1))-1; i++) {
+            tree[i * 2 + 1] = 0;
+            tree[i * 2 + 2] = 1;
+        }
+        inOrder(tree, 0);
+    }
+
+    public static void main(String[] args) {
+        solution(3);
+    }
+}
