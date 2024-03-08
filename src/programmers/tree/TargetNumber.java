b17d557801292546104763f429a68a0fa4664ad9
kjw2948 <kjw970313@cau.ac.kr>
2024-03-08 21:42:53 +0900

    프로그래머스 bfs/dfs 타겟넘버

---
+++ src/programmers/tree/TargetNumber.java
@@ -0,0 +1,29 @@
+package programmers.tree;
+
+import java.util.ArrayList;
+
+public class TargetNumber {
+    static int answer = 0;
+    public static int solution(int[] numbers, int target) {
+        int sum = 0;
+        dfs(numbers, 0, target, sum);
+        return answer;
+    }
+    public static void dfs(int[] numbers, int depth, int target, int sum) {
+        if (numbers.length == depth) {
+            if(target == sum) {
+                answer++;
+            }
+        }
+        else {
+            dfs(numbers, depth + 1, target, sum + numbers[depth]);
+            dfs(numbers, depth + 1, target, sum - numbers[depth]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 3;
+        System.out.println(solution(numbers, target));
+    }
+}
