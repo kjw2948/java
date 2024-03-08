3049e9b80335b39ef5de642fc7c732e52c6e923b
kjw2948 <kjw970313@cau.ac.kr>
2024-03-08 13:26:58 +0900

    트리 순회 구현

--- src/datastructure/tree/Practice2.java
+++ src/datastructure/tree/Practice2.java
@@ -1,14 +1,47 @@
 package datastructure.tree;
 
 class Node{
-    char data;
+    int data;
     Node left;
     Node right;
+}
 
-    public Node(char data, Node left, Node right) {
-        this.data = data;
-        this.left = left;
-        this.right = right;
+class Tree {
+    public Node root;
+    public void setRoot(Node node) {
+        this.root = node;
+    }
+    public Node getRoot() {
+        return root;
+    }
+
+    public Node makeNode(int data, Node left, Node right) {
+        Node node = new Node();
+        node.data = data;
+        node.left = left;
+        node.right = right;
+        return node;
+    }
+    public void inOrder(Node node) {
+        if(node!=null){
+            inOrder(node.left);
+            System.out.print(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+    public void preOrder(Node node) {
+        if(node!=null){
+            System.out.print(node.data + " ");
+            inOrder(node.left);
+            inOrder(node.right);
+        }
+    }
+    public void postOrder(Node node) {
+        if(node!=null){
+            inOrder(node.left);
+            inOrder(node.right);
+            System.out.print(node.data + " ");
+        }
     }
 }
 
@@ -16,5 +49,22 @@ class BinaryTree2 {
     Node head;
 }
 public class Practice2 {
-
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        Node n4 = tree.makeNode(4, null, null);
+        Node n5 = tree.makeNode(5, null, null);
+        Node n2 = tree.makeNode(2, n4, n5);
+        Node n3 = tree.makeNode(3, null, null);
+        Node n1 = tree.makeNode(1, n2, n3);
+        tree.setRoot(n1);
+        System.out.println("====== preOrder ======");
+        tree.preOrder(n1);
+        System.out.println("");
+        System.out.println("====== inOrder ======");
+        tree.inOrder(n1);
+        System.out.println("");
+        System.out.println("====== postOrder ======");
+        tree.postOrder(n1);
+        System.out.println("");
+    }
 }
---
+++ src/datastructure/tree/bst/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package datastructure.tree.bst;
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+}
+public class BinarySearchTree {
+}
