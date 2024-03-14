adbf61077479985fd8f0632c47f5f429d361f741
kjw2948 <kjw970313@cau.ac.kr>
2024-03-14 20:51:17 +0900

    코테 구현 / 탑 쌓기

---
+++ src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Solution {
+    public static int combination(int N, int num_one) {
+        int answer = 1;
+        if(num_one<0){
+            return 0;
+        }
+        for (int i = N; i > N-num_one; i--) {
+            answer *= i;
+        }
+        for (int i = 1; i <= num_one; i++) {
+            answer /= i;
+        }
+        return answer + combination(N-1, num_one-2);
+    }
+    public static int solution(int N) {
+        int answer = 0;
+        answer = combination(N, N);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] names = {""};
+        int N = 6;
+        System.out.println(solution(N));
+    }
+}
