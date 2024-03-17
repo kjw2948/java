21f3f144ffeff87f0b5334d67d004eaf408607d5
kjw2948 <kjw970313@cau.ac.kr>
2024-03-17 23:00:00 +0900

    프로그래머스 day8-3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static String solution(int age) {
-        String answer = "";
-        String str = Integer.toString(age);
-        for (int i = 0; i < str.length(); i++) {
-            int c = (int) str.charAt(i) + '1';
-            answer += (char) c;
+    public static int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+
+        for (int i = 0; i < emergency.length; i++) {
+            for (int j = 0; j < emergency.length; j++) {
+                if (emergency[i] < emergency[j]) {
+                    answer[i]++;
+                }
+            }
+
+            answer[i]++;
         }
+
         return answer;
     }
     public static void main(String[] args) {
         int age = 23;
-        System.out.println(solution(age));
+        int[] emergency = {3, 17, 6};
+        int[] answer = solution(emergency);
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i]);
+        }
     }
 }
