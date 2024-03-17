5180c035d86a10c35b6c3958744e26ba6257ea13
kjw2948 <kjw970313@cau.ac.kr>
2024-03-17 23:08:15 +0900

    프로그래머스 day8-4

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,18 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static int[] solution(int[] emergency) {
-        int[] answer = new int[emergency.length];
-
-        for (int i = 0; i < emergency.length; i++) {
-            for (int j = 0; j < emergency.length; j++) {
-                if (emergency[i] < emergency[j]) {
-                    answer[i]++;
-                }
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer++;
             }
-
-            answer[i]++;
         }
-
         return answer;
     }
     public static void main(String[] args) {
         int age = 23;
         int[] emergency = {3, 17, 6};
-        int[] answer = solution(emergency);
-        for (int i = 0; i < answer.length; i++) {
-            System.out.print(answer[i]);
-        }
+        System.out.println(solution(20));
     }
 }
