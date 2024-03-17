025b8b44a5d29d35c0efdcf5292fb793d21bd5e0
kjw2948 <kjw970313@cau.ac.kr>
2024-03-17 23:12:43 +0900

    프로그래머스 day9-1

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static int solution(int n) {
+    public static int solution(int hp) {
         int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                answer++;
-            }
+        int[] ant = {5, 3, 1};
+        for (int i = 0; i < ant.length; i++) {
+            answer += hp / ant[i];
+            hp %= ant[i];
         }
         return answer;
     }
     public static void main(String[] args) {
         int age = 23;
         int[] emergency = {3, 17, 6};
-        System.out.println(solution(20));
+        System.out.println(solution(23));
     }
 }
