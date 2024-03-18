57befda2ebe6b548efecdabf7e3edc158862c825
kjw2948 <kjw970313@cau.ac.kr>
2024-03-18 20:09:15 +0900

    프로그래머스 day10-2

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,22 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static int solution(int[] dot) {
-        if (dot[0] > 0 && dot[1] > 0) {
-            return 1;
-        } else if (dot[0] < 0 && dot[1] > 0) {
-            return 2;
-        } else if (dot[0] < 0 && dot[1] < 0) {
-            return 3;
-        } else {
-            return 4;
+    public static int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length / n][n];
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i / n][i % n] = num_list[i];
         }
+        return answer;
     }
     public static void main(String[] args) {
         int age = 23;
-        int[] emergency = {3, 17};
-        System.out.println(solution(emergency));
+        int[] num_list = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[][] answer = solution(num_list, 2);
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[i].length; j++) {
+                System.out.print(answer[i][j]+" ");
+            }
+            System.out.println("");
+        }
     }
 }
