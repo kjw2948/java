e1004a78fd94e14962a158fcc8f5884e330dab73
kjw2948 <kjw970313@cau.ac.kr>
2024-03-18 19:35:25 +0900

    프로그래머스 day10-1

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,20 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static int solution(int balls, int share) {
-        if (share == 0 || balls == share)
+    public static int solution(int[] dot) {
+        if (dot[0] > 0 && dot[1] > 0) {
             return 1;
-        else
-            return solution(balls-1, share - 1) + solution(balls - 1, share);
+        } else if (dot[0] < 0 && dot[1] > 0) {
+            return 2;
+        } else if (dot[0] < 0 && dot[1] < 0) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
     public static void main(String[] args) {
         int age = 23;
-        int[] emergency = {3, 17, 6};
-        System.out.println(solution(5,3));
+        int[] emergency = {3, 17};
+        System.out.println(solution(emergency));
     }
 }
