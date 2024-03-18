98b3bc0bfe18ebfd660acfdc0742e9202600905e
kjw2948 <kjw970313@cau.ac.kr>
2024-03-18 19:22:01 +0900

    프로그래머스 day9-2

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,8 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static String solution(String letter) {
+    public static String solution(String rsp) {
         String answer = "";
-        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        String[] morseArr = letter.split(" ");
-        for (String s : morseArr) {
-            for (int i = 0; i < morse.length; i++) {
-                if (s.equals(morse[i])) {
-                    answer += Character.toString(i + 'a');
-                }
-            }
-        }
         return answer;
     }
     public static void main(String[] args) {
