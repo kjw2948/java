98a8c5d46a468a523cb7ba36a57552233a51096c
kjw2948 <kjw970313@cau.ac.kr>
2024-03-18 20:34:12 +0900

    프로그래머스 기본 day1-3

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static int[][] solution(int[] num_list, int n) {
-        int[][] answer = new int[num_list.length / n][n];
-        for (int i = 0; i < num_list.length; i++) {
-            answer[i / n][i % n] = num_list[i];
-        }
+    public static int solution(int[] numbers, int k) {
+        int answer = 0;
         return answer;
     }
     public static void main(String[] args) {
-        int age = 23;
-        int[] num_list = {1, 2, 3, 4, 5, 6, 7, 8};
-        int[][] answer = solution(num_list, 2);
-        for (int i = 0; i < answer.length; i++) {
-            for (int j = 0; j < answer[i].length; j++) {
-                System.out.print(answer[i][j]+" ");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String answer = "";
+        for(int i = 0; i< a.length();i++){
+            char c = a.charAt(i);
+            if(Character.isUpperCase(c)){
+                answer += Character.toLowerCase(c);
+            } else{
+                answer += Character.toUpperCase(c);
             }
-            System.out.println("");
         }
+        System.out.println(answer);
     }
 }
