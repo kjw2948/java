4b44b91061a85cf9787be6e242c8e06c41eeb394
kjw2948 <kjw970313@cau.ac.kr>
2024-03-18 19:29:05 +0900

    프로그래머스 day9-4

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,15 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static String solution(String rsp) {
-        String answer = "";
-        for (int i = 0; i < rsp.length(); i++) {
-            if (rsp.charAt(i) == '0') {
-                answer += "5";
-            } else if (rsp.charAt(i) == '2') {
-                answer += "0";
-            } else {
-                answer += "2";
-            }
-        }
-        return answer;
+    public static int solution(int balls, int share) {
+        if (share == 0 || balls == share)
+            return 1;
+        else
+            return solution(balls-1, share - 1) + solution(balls - 1, share);
     }
     public static void main(String[] args) {
         int age = 23;
         int[] emergency = {3, 17, 6};
-        System.out.println(solution(".... . .-.. .-.. ---"));
+        System.out.println(solution(5,3));
     }
 }
