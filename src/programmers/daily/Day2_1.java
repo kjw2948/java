af9a3ce4e464b41dfdb569f7dc5f3eb02fce701e
kjw2948 <kjw970313@cau.ac.kr>
2024-03-19 20:48:01 +0900

    프로그래머스 기본 day2-1

---
+++ src/programmers/daily/Day2_1.java
@@ -0,0 +1,13 @@
+package programmers.daily;
+
+import java.util.Scanner;
+
+public class Day2_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String str = a+b;
+        System.out.println(str);
+    }
+}
