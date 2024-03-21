e9ad7dfb273ef3565e1941ed6eb7198032edd705
kjw2948 <kjw970313@cau.ac.kr>
2024-03-21 16:43:39 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,6 @@
 import java.util.*;
 
 public class Main {
-    public int solution(int[] numbers) {
-        int answer = 0;
-        Arrays.sort(numbers);
-        int num = numbers[0];
-        for (int i = 1; i < numbers.length; i++) {
-            num++;
-            if (num != numbers[i]) {
-                answer = numbers[i-1]+1;
-                break;
-            }
-
-        }
-        return answer;
-    }
     public static void main(String[] args) {
     }
 }
