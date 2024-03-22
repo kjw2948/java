abafa1ad0a312fac6442c5c5b4c2aab251ea81cf
kjw2948 <kjw970313@cau.ac.kr>
2024-03-22 19:38:16 +0900

    카카오 Internship2019

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,52 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static int[] solution(int[] numbers) {
-        Arrays.sort(numbers);
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i<numbers.length; i++){
-            set.add(numbers[i]);
-        }
-        int[] number = new int[set.size()];
-        int[] answer = new int[set.size()*(set.size()-1)/2];
-        Iterator<Integer> iter1 = set.iterator();
-        int index = 0;
-        while (iter1.hasNext()) {
-            number[index++] = iter1.next();
-        }
-        index = 0;
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i+1; j < number.length; j++) {
-                answer[index++] = number[i] + number[j];
+    public static int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int[] complete = new int[N];
+        int[] step = new int[N];
+        double[] fail = new double[N];
+        for(int i = 0; i<stages.length; i++){
+            if(stages[i]==N+1){
+                break;
+            }else {
+                for (int j = 0; j < stages[i]; j++) {
+                    complete[j]++;
+                }
             }
         }
+        for(int i = 0; i<stages.length; i++){
+            if(stages[i]==N+1){
+                break;
+            }else {
+                for (int j = 0; j <= stages[i]; j++) {
+                    step[j]++;
+                }
+            }
+        }
+        for(int i =0; i<N; i++){
+            if(step[i]==0){
+                fail[i] = 0;
+            }else{
+                fail[i] = (double)((step[i] - complete[i])) / step[i];
+            }
+        }
+        double[] sortFail = fail.clone();
+        Arrays.sort(sortFail);
+
+        for(int i = 0; i<N; i++){
+            answer[i] = Arrays.stream(fail).boxed().collect(Collectors.toList())
+                    .indexOf(sortFail[i]) + 1;
+        }
+
         return answer;
     }
     public static void main(String[] args) {
-        int[] arr = {2,1,3,4,1};
-        int[] answer = solution(arr);
+        int[] arr = {2, 1, 2, 6, 2, 4, 3, 3};
+        int[] answer = solution(5,arr);
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i] +" ");
+        }
     }
 }
