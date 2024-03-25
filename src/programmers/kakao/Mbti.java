315b14c607365e3c6bf0b19baa95af5a0226da1b
kjw2948 <kjw970313@cau.ac.kr>
2024-03-25 20:37:44 +0900

    카카오 2022 mbti

---
+++ src/programmers/kakao/Mbti.java
@@ -0,0 +1,13 @@
+package programmers.kakao;
+
+public class Mbti {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
