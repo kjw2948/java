26fe5d255b65d06374e5f7847cbff26b1708a704
kjw2948 <kjw970313@cau.ac.kr>
2024-03-26 13:13:52 +0900

    메소드 생성

--- src/oop/driver/Driver.java
+++ src/oop/driver/Driver.java
@@ -1,4 +1,15 @@
 package oop.driver;
 
 public class Driver {
+    private K3Car k3Car;
+    void setK3Car(K3Car k3Car){
+        this.k3Car = k3Car;
+    }
+    void drive() {
+        System.out.println("자동차를 운전합니다");
+        k3Car.startEngine();
+        k3Car.offEngine();
+        k3Car.pressAccelator();
+
+    }
 }
--- src/oop/driver/K3Car.java
+++ src/oop/driver/K3Car.java
@@ -1,4 +1,14 @@
 package oop.driver;
 
 public class K3Car {
+    public void startEngine() {
+        System.out.println("K3Car.startEngine");
+    }
+    public void offEngine() {
+        System.out.println("K3Car.offEngine");
+    }
+
+    public void pressAccelator() {
+        System.out.println("K3Car.pressAccelator");
+    }
 }
