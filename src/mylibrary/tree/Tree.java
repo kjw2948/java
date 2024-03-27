adbb6abb511042a096381cd33852e3b32f1f91e4
kjw2948 <kjw970313@cau.ac.kr>
2024-03-27 19:00:02 +0900

    [Update] 트리 순회 추가

--- src/mylibrary/tree/Tree.java
+++ src/mylibrary/tree/Tree.java
@@ -1,7 +1,34 @@
 package mylibrary.tree;
 
 class Node {
-
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+    }
 }
 public class Tree {
+    static void preOrder(Node node) {
+        if (node != null) {
+            System.out.println(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+    static void inOrder(Node node) {
+        if (node != null) {
+            preOrder(node.left);
+            System.out.println(node.data + " ");
+            preOrder(node.right);
+        }
+    }
+    static void postOrder(Node node) {
+        if (node != null) {
+            preOrder(node.left);
+            preOrder(node.right);
+            System.out.println(node.data + " ");
+        }
+    }
+
 }
