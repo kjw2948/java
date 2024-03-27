db7857b910d445918f012c9cec237b2fa3edc33d
kjw2948 <kjw970313@cau.ac.kr>
2024-03-27 16:44:55 +0900

    소수찾기

---
+++ src/programmers/dfsbfs/PrimeNumber.java
@@ -0,0 +1,21 @@
+package programmers.dfsbfs;
+
+public class PrimeNumber {
+    public static int solution(String numbers) {
+        int answer = 0;
+        int index = 0;
+        while(index<numbers.length()){
+            for (int i = 0; i < numbers.length() - index; i++) {
+                String str = "";
+                for (int j = 0; j < numbers.length()-index&&j!=i; j++) {
+                    
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
