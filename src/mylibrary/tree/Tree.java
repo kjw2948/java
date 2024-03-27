02530f5c9e702a5a8d9c3b7b236a253ab96f999a
kjw2948 <kjw970313@cau.ac.kr>
2024-03-27 19:20:18 +0900

    [Update] 트리추가 후 순회 결과 출력

--- src/mylibrary/tree/Tree.java
+++ src/mylibrary/tree/Tree.java
@@ -1,5 +1,7 @@
 package mylibrary.tree;
 
+import java.util.ArrayList;
+
 class Node {
     int data;
     Node left;
@@ -8,26 +10,50 @@ class Node {
         this.data = data;
     }
 }
+
 public class Tree {
+    public static void main(String[] args) {
+        int count = 7;
+        Node[] tree = new Node[count+1];
+        for (int i = 1; i <= count; i++) {
+            Node node = new Node(i);
+            tree[i] = node;
+        }
+        for (int i = 1; i*2 <= count; i++) {
+            tree[i].left = tree[i*2];
+            tree[i].right = tree[i*2 + 1];
+        }
+        System.out.println("---------preOrder---------");
+        preOrder(tree[1]);
+        System.out.println(" ");
+        System.out.println("---------inOrder---------");
+        inOrder(tree[1]);
+        System.out.println(" ");
+        System.out.println("---------postOrder---------");
+        postOrder(tree[1]);
+    }
+
     static void preOrder(Node node) {
         if (node != null) {
-            System.out.println(node.data + " ");
+            System.out.print(node.data + " ");
             preOrder(node.left);
             preOrder(node.right);
         }
     }
+
     static void inOrder(Node node) {
         if (node != null) {
-            preOrder(node.left);
-            System.out.println(node.data + " ");
-            preOrder(node.right);
+            inOrder(node.left);
+            System.out.print(node.data + " ");
+            inOrder(node.right);
         }
     }
+
     static void postOrder(Node node) {
         if (node != null) {
-            preOrder(node.left);
-            preOrder(node.right);
-            System.out.println(node.data + " ");
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.print(node.data + " ");
         }
     }
 
