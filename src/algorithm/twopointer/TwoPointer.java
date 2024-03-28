b3872b9495d92b8c6ff57384c8c24b76503e6ee6
kjw2948 <kjw970313@cau.ac.kr>
2024-03-28 21:36:10 +0900

    [Update] 투포인터 target number 찾기

--- src/algorithm/twopointer/TwoPointer.java
+++ src/algorithm/twopointer/TwoPointer.java
@@ -1,32 +1,30 @@
 package algorithm.twopointer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TwoPointer {
     public static int[] twoPointer(int[] arr, int target){
-        int[] result = {-1, -1};
-        int p1 = 0;
-        int p2 = 0;
+        ArrayList<Integer> answer = new ArrayList<>();
+        int start = 0;
+        int end = 0;
         int sum = 0;
         while(true){
-            if(sum > target){
-                sum -= arr[p1++];
-            } else if (p2 == arr.length) {
+            if(sum<target){
+                sum += arr[start];
+                answer.add(arr[start++]);
+            } else if (sum == target) {
                 break;
-            } else{
-                sum += arr[p2++];
-            }
-            if(sum == target){
-                result[0] = p1;
-                result[1] = p2-1;
-                break;
+            }else{
+                sum -= arr[end++];
+                answer.remove(0);
             }
         }
-        return result;
+        return answer.stream().mapToInt(i->i).toArray();
     }
     public static void main(String[] args) {
         int[] arr = {1, 2, 5, 3, 7, 2, 4, 3, 2};
-        int target = 9;
+        int target = 10;
         System.out.println(Arrays.toString(twoPointer(arr, target)));
     }
 }
