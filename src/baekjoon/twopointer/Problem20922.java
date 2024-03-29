15d859f9266d61ad7b5ebf0110350e2fc8bb743f
kjw2948 <kjw970313@cau.ac.kr>
2024-03-30 00:12:16 +0900

    [Update] init

--- src/baekjoon/twopointer/Problem20922.java
+++ src/baekjoon/twopointer/Problem20922.java
@@ -1,7 +1,10 @@
 package baekjoon.twopointer;
 // 실버1
 public class Problem20922 {
+    public static int solution() {
+        return 0;
+    }
     public static void main(String[] args) {
-
+        
     }
 }
