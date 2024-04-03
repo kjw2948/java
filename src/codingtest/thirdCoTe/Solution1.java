ce9015d7c2f95880daa11ed1e4494c146017c341
kjw2948 <kjw970313@cau.ac.kr>
2024-04-03 20:52:24 +0900

    [Update] 코테 1번

--- src/codingtest/thirdCoTe/Solution1.java
+++ src/codingtest/thirdCoTe/Solution1.java
@@ -4,36 +4,25 @@ import java.util.HashMap;
 
 public class Solution1 {
     public static int solution(int N, int[][] trust) {
-        int answer = 0;
-        HashMap<Integer, Integer> result = new HashMap<>();
-        if(trust.length==0)
-        for (int i = 1; i < N+1; i++) {
-            result.put(i, 0);
-        }
+        int[] a = new int[N+1];
+        int[] b = new int[N+1];
         for (int i = 0; i < trust.length; i++) {
-            result.put(trust[i][0], result.getOrDefault(trust[i][0], 0) + 1);
-            result.put(trust[i][1], result.getOrDefault(trust[i][1], 0) + 100);
+            a[trust[i][0]]++;
+            b[trust[i][1]]++;
         }
-        int count = 0;
-        for (Integer integer : result.keySet()) {
-            if(result.get(integer)>=100){
-                count ++;
-            }
-            if(result.get(integer) % 100 == 0){
-                answer = integer;
-            }
-            if(count==N){
-                answer = 0;
+        for (int i = 1; i <= N; i++) {
+            if(a[i]==0 && b[i]==N-1){
+                return i;
             }
         }
-        return answer == 0 ? -1 : answer;
+        return -1;
     }
 
     public static void main(String[] args) {
-        int[][] arr = {{1, 2}, {1, 4}, {1, 3}, {2, 1}, {4, 3}};
-        //int[][] arr ={{1,3},{2,3}};
+        int[][] arr = {{1, 2}, {1, 4}, {1, 3}, {2, 1}, {4, 3}, {2,3}};
+        int[][] arr1 ={{1,3},{2,3}};
 
-        System.out.println(solution(4, arr));
+        System.out.println(solution(3, arr1));
 
     }
 }
