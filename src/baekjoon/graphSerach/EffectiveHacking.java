e573e1f4fdd7b4ca61484d66a06fb8b4fd03e551
kjw2948 <kjw970313@cau.ac.kr>
2024-04-28 13:18:12 +0900

    [Update] 효율적인 해킹

--- src/baekjoon/graphSerach/EffectiveHacking.java
+++ src/baekjoon/graphSerach/EffectiveHacking.java
@@ -1,58 +1,56 @@
 package baekjoon.graphSerach;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class EffectiveHacking {
-    static boolean[] visit;
-    static ArrayList<Integer>[] list;
+    static boolean[] visited;
+    static ArrayList<Integer>[] listArr;
     static int[] cntArr;
-    static int max;
-    public static void bfs(int idx) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        visit[idx] = true;
-        queue.add(idx);
-        while (!queue.isEmpty()) {
-            int a = queue.poll();
-            for (Integer i : list[a]) {
-                if(visit[i]) continue;
-                queue.add(i);
-                visit[i] = true;
-                cntArr[i] ++;
-            }
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        list = new ArrayList[N+1];
-        cntArr = new int[N + 1];
-        for (int i = 0; i < N+1; i++) {
-            list[i] = new ArrayList<>();
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        listArr = new ArrayList[N+1];
+        for (int i = 0; i < listArr.length; i++) {
+            listArr[i] = new ArrayList<>();
         }
+
+        cntArr = new int[N + 1];
+        visited = new boolean[N + 1];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            list[A].add(B);
-        }
-        for (int i = 1; i <= N; i++) {
-            visit = new boolean[N+1];
-            bfs(i);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            listArr[a].add(b);
         }
 
-        for (int i=1; i<N+1; i++) {
-            if (max<cntArr[i]) max = cntArr[i];
-        }
 
-        // 최댓값인 컴퓨터 출력
-        for (int i=1; i<N+1; i++) if (max == cntArr[i]) System.out.print(i+" ");
     }
+    public static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
 
+        while (!queue.isEmpty()) {
+            int n = queue.poll();
+            for (int i : listArr[n]) {
+                if(!visited[i]){
+                    queue.add(i);
+                    visited[i] = true;
+                    cntArr[i] ++;
+                }
+            }
+        }
+    }
 }
