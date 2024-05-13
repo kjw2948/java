f8f72baf1e62659731d35caa4de289fbdb521eb8
kjw2948 <kjw970313@cau.ac.kr>
2024-05-13 14:04:19 +0900

    [Update] 백준 2504번

--- src/baekjoon/implementation/Problem2504.java
+++ src/baekjoon/implementation/Problem2504.java
@@ -5,29 +5,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.StringTokenizer;
 
 public class Problem2504 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Deque<Character> stack = new ArrayDeque<>();
-        Deque<Integer> result = new ArrayDeque<>();
+
+        int result = 0 ;
+        int value = 1;
 
         char[] inputArray = br.readLine().toCharArray();
-        int index = 0;
-        char check = ' ';
-        while (index<inputArray.length) {
-            char input = inputArray[index++];
-            switch (input){
-                case '(', '[' :
-                    stack.add(input);
-                    break;
-                case ')':
-                    if(check=='('){
-                        
-                    }
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == '(') {
+                stack.add(inputArray[i]);
+                value *= 2;
+            } else if (inputArray[i] == '[') {
+                stack.add(inputArray[i]);
+                value *= 3;
+            } else if (inputArray[i] == ')') {
+                if (stack.isEmpty() || stack.peekLast() != '(') {
+                    System.out.println(0);
+                    return;
+                }
+                if (inputArray[i - 1] == '(') {
+                    result += value;
+                }
+                stack.pollLast();
+                value /= 2;
+            } else if (inputArray[i] == ']') {
+                if (stack.isEmpty() || stack.peekLast() != '[') {
+                    System.out.println(0);
+                    return;
+                }
+                if (inputArray[i - 1] == '[') {
+                    result += value;
+                }
+                stack.pollLast();
+                value /= 3;
             }
         }
-        System.out.println(result.poll());
+
+
+        if (!stack.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+        System.out.println(result);
     }
 }
