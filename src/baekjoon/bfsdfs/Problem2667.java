c30f2775dcbbbe4eda72d81d418572eb327f75cd
kjw2948 <kjw970313@cau.ac.kr>
2024-05-16 14:27:54 +0900

    [Update] 복습

--- src/baekjoon/bfsdfs/Problem2667.java
+++ src/baekjoon/bfsdfs/Problem2667.java
@@ -1,37 +1,38 @@
 package baekjoon.bfsdfs;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 public class Problem2667 {
     static boolean[][] visited;
     static int[][] arr;
     static int[][] pos = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    static int count = 1;
     static ArrayList<Integer> result = new ArrayList<>();
+    static int count = 1;
+    static int N;
+
     public static void dfs(int x, int y) {
         visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
             int cur_x = x + pos[i][0];
             int cur_y = y + pos[i][1];
-            if (cur_x >= 0 && cur_y >= 0 && cur_x < arr.length && cur_y < arr.length
-                    && arr[cur_x][cur_y] == 1 && !visited[cur_x][cur_y]) {
-                dfs(cur_x, cur_y);
+            if (cur_x >= 0 && cur_y >= 0 && cur_x < N && cur_y < N && arr[cur_x][cur_y] == 1 && !visited[cur_x][cur_y]) {
                 count ++;
+                dfs(cur_x, cur_y);
             }
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         arr = new int[N][N];
         visited = new boolean[N][N];
-
 
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
@@ -42,7 +43,7 @@ public class Problem2667 {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if(arr[i][j] == 1 && !visited[i][j]){
+                if (arr[i][j] == 1 && !visited[i][j]) {
                     dfs(i, j);
                     result.add(count);
                     count = 1;
@@ -50,9 +51,8 @@ public class Problem2667 {
             }
         }
         Collections.sort(result);
-        int length = result.size();
-        System.out.println(length);
-        for (int i = 0; i < length; i++) {
+        System.out.println(result.size());
+        for (int i = 0; i < result.size(); i++) {
             System.out.println(result.get(i));
         }
     }
