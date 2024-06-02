897665490343a776e738bed90153646635b3bf84
kjw2948 <kjw970313@cau.ac.kr>
2024-06-02 18:36:27 +0900

    [Update] generic 사용

--- src/generic/ex1/BoxMain3.java
+++ src/generic/ex1/BoxMain3.java
@@ -3,12 +3,12 @@ package generic.ex1;
 public class BoxMain3 {
 
     public static void main(String[] args) {
-        GenericBox<Integer> integerBox = new GenericBox<Integer>(); // 생성시점에 T의 타입 결정
+        GenericBox<Integer> integerBox = new GenericBox<>(); // 생성시점에 T의 타입 결정
         integerBox.set(10);
         Integer integer = integerBox.get();
         System.out.println("integer = " + integer);
 
-        GenericBox<String> stringBox = new GenericBox<String>();
+        GenericBox<String> stringBox = new GenericBox<>();
         stringBox.set("hello generic");
         String str = stringBox.get();
         System.out.println("str = " + str);
