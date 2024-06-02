7fd44771145de478cdbf8cb1068f9e70b2ee3777
kjw2948 <kjw970313@cau.ac.kr>
2024-06-02 18:10:55 +0900

    [Update] generic

---
+++ src/generic/ex1/ObjectBox.java
@@ -0,0 +1,14 @@
+package generic.ex1;
+
+public class ObjectBox {
+
+    private Object value;
+    public void set(Object o){
+        this.value = o;
+    }
+
+    public Object get() {
+        return value;
+    }
+
+}
