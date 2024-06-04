3600e497057096294c1770a7393571e526b42d6b
kjw2948 <kjw970313@cau.ac.kr>
2024-06-04 15:38:44 +0900

    [Update] Update

--- src/Main.java
+++ src/Main.java
@@ -11,42 +11,8 @@ public class Main{
         int[] checkCount = new int[8];
         int index = 0;
         int count = 0;
-        /*
-        while(index<8){
-            for (int i = 0; i < 6; i++) {
-                correct += Integer.toString(random.nextInt(0, 10));
-            }
-            count ++;
-            if(correct.equals(recent[index])){
-                checkCount[index] = count;
-                count = 0;
-                index ++;
-            }
-            correct = "";
-        }
-        int sum = 0;
-        for (int i = 0; i < checkCount.length; i++) {
-            sum += checkCount[i];
-        }
-        int next = sum / 8;
-        for (int i = 0; i < next; i++) {
-            for (int j = 0; j < 6; j++) {
-               Integer.toString(random.nextInt(0, 10));
-            }
-            if(i==next-1){
-                for (int j = 0; j < 6; j++) {
-                    correct += Integer.toString(random.nextInt(0, 10));
-                }
-            }
-        }
-        String prefix = Integer.toString(random.nextInt(1, 6));
-        System.out.println("prefix = " + prefix);
-        System.out.println("Last correct = " + prefix + correct);
-
-
-         */
-        //sevenTwentyPlus();
-        sixFourFive();
+        sevenTwentyPlus();
+        //sixFourFive();
     }
 
     public static void sixFourFive() {
---
+++ src/generic/ex4/MethodMain2.java
@@ -0,0 +1,15 @@
+package generic.ex4;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class MethodMain2 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("멍멍이", 100);
+        Cat cat = new Cat("야옹이", 300);
+
+        AnimalMethod.checkUp(dog);
+        AnimalMethod.checkUp(cat);
+    }
+}
