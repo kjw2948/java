25cc2ffc60f4879d8e851c9ea772ebbafee708f6
kjw2948 <kjw970313@cau.ac.kr>
2024-06-12 16:15:12 +0900

    [Update] 내가 만든 객체 -> 나이순 정렬

--- src/collection/compare/MyUser.java
+++ src/collection/compare/MyUser.java
@@ -1,11 +1,42 @@
 package collection.compare;
 
-public class MyUser {
+import java.util.Comparator;
+
+public class MyUser implements Comparable<MyUser>{
     private String id;
     private int age;
 
     public MyUser(String id, int age) {
         this.id = id;
         this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(MyUser user1) {
+        return (this.age < user1.getAge() ? -1 : (this.age == user1.getAge() ? 0 : 1));
+    }
+
+    @Override
+    public String toString() {
+        return "MyUser{" +
+                "id='" + id + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
---
+++ src/collection/compare/SortMain3.java
@@ -0,0 +1,20 @@
+package collection.compare;
+
+import java.util.Arrays;
+
+public class SortMain3 {
+    public static void main(String[] args) {
+        MyUser user1 = new MyUser("a", 10);
+        MyUser user2 = new MyUser("b", 30);
+        MyUser user3 = new MyUser("c", 20);
+
+        MyUser[] array = {user1, user2, user3};
+        System.out.println("기본 데이터");
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("Comparable 기본 정렬");
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}
