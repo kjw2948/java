caa4bcd818ba2066424b11e339dbd2d437745e46
kjw2948 <kjw970313@cau.ac.kr>
2024-06-14 15:28:14 +0900

    [Update] 백준 15686

--- src/baekjoon/implementation/Problem15686.java
+++ src/baekjoon/implementation/Problem15686.java
@@ -3,19 +3,32 @@ package baekjoon.implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Problem15686 {
     static int[][] map;
-    static Map<Integer, Integer> house = new HashMap<>();
-    static Map<Integer, Integer> chicken = new HashMap<>();
+    static List<String> house = new ArrayList<>();
+    static List<String> chicken = new ArrayList<>();
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
         map = new int[N][N];
-        int M = Integer.parseInt(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    house.add(Integer.toString(i) + " " +Integer.toString(j));
+                }
+                if (map[i][j] == 2) {
+                    chicken.add(Integer.toString(i) + " " +Integer.toString(j));
+                }
+            }
+        }
 
 
     }
