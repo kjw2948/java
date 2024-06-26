8a9758f1ea41eb186657018be6a5bd36d6b92288
kjw2948 <kjw970313@cau.ac.kr>
2024-06-26 20:56:41 +0900

    [Update] 백준 16719

--- src/baekjoon/implementation/Problem16719.java
+++ src/baekjoon/implementation/Problem16719.java
@@ -5,38 +5,37 @@ import java.io.*;
 public class Problem16719 {
     static StringBuilder sb= new StringBuilder();
     static String word;
-    static boolean [] appear;//등장할 친구인지 아닌지
+    static boolean [] appear;
     public static void main(String []args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         word = br.readLine();
-        appear = new boolean[word.length()];
 
-        Zoac(0, word.length()-1);
-        br.close();
+        appear = new boolean[word.length()];
+        zoac(0, word.length() - 1);
         System.out.println(sb);
+
+
     }
-    private static void Zoac(int left, int right) {
-        //종료 기준 마련
-        if(left>right) {
+    public static void zoac(int left, int right) {
+        if (left > right) {
             return;
         }
-        //사전순으로 가장빠른 문자를 찾아서 boolean배열에서
-        //그 값을 true로 만들어주자
-        int min = left;
-        for(int i=left; i<=right; i++) {
-            if(word.charAt(min)>word.charAt(i)) {
-                min = i;
+
+        int idx = left;
+        for (int i = left; i <= right; i++) {
+            if (word.charAt(idx) > word.charAt(i)) {
+                idx = i;
             }
         }
-        appear[min] =true;
-        //make 구문
-        for(int i=0; i<word.length(); i++) {
-            if(appear[i]) {
+        appear[idx] = true;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (appear[i]) {
                 sb.append(word.charAt(i));
             }
         }
         sb.append("\n");
-        Zoac(min+1, right);
-        Zoac(left, min-1);
+        zoac(idx + 1, right);
+        zoac(left, idx - 1);
     }
 }
