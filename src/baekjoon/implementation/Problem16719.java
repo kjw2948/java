663394843a590c2a821f79e50e81866af2ba7bc8
kjw2948 <kjw970313@cau.ac.kr>
2024-06-26 17:01:54 +0900

    [Update] 백준 16719

--- src/baekjoon/implementation/Problem16719.java
+++ src/baekjoon/implementation/Problem16719.java
@@ -1,28 +1,42 @@
 package baekjoon.implementation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
 
 public class Problem16719 {
-    static List<Integer>[] arr; //
-    static Map<Integer, Character> map = new HashMap<>(); // 입력 문자열에서 위치, 해당 위치의 값
-
-    public static void main(String[] args) throws IOException {
+    static StringBuilder sb= new StringBuilder();
+    static String word;
+    static boolean [] appear;//등장할 친구인지 아닌지
+    public static void main(String []args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        word = br.readLine();
+        appear = new boolean[word.length()];
 
-
-        //bac => 1 0 2 // 1 0 2 // a b c
-        for (int i = 0; i < str.length(); i++) {
-            char a = str.charAt(i);
-            int idx = a - 'a'; // 알파벳 인덱스
-
-            map.put(i, a);
+        Zoac(0, word.length()-1);
+        br.close();
+        System.out.println(sb);
+    }
+    private static void Zoac(int left, int right) {
+        //종료 기준 마련
+        if(left>right) {
+            return;
         }
+        //사전순으로 가장빠른 문자를 찾아서 boolean배열에서
+        //그 값을 true로 만들어주자
+        int min = left;
+        for(int i=left; i<=right; i++) {
+            if(word.charAt(min)>word.charAt(i)) {
+                min = i;
+            }
+        }
+        appear[min] =true;
+        //make 구문
+        for(int i=0; i<word.length(); i++) {
+            if(appear[i]) {
+                sb.append(word.charAt(i));
+            }
+        }
+        sb.append("\n");
+        Zoac(min+1, right);
+        Zoac(left, min-1);
     }
 }
