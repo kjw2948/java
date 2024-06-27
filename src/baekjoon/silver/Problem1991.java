b5faec7f1c262c696b7e5ea04043c6ba479534fc
kjw2948 <kjw970313@cau.ac.kr>
2024-06-27 21:03:50 +0900

    [Update] 백준 1991번

--- src/baekjoon/silver/Problem1991.java
+++ src/baekjoon/silver/Problem1991.java
@@ -3,68 +3,67 @@ package baekjoon.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node{
-    char data;
-    Node left;
-    Node right;
-    public Node(char data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-}
 public class Problem1991 {
-    static Node[] tree;
-    public static void inOrder(Node node) {
-        if(node!=null){
-            inOrder(node.left);
-            System.out.print(node.data);
-            inOrder(node.right);
-        }
-    }
-    public static void preOrder(Node node) {
-        if(node!=null){
-            System.out.print(node.data);
-            preOrder(node.left);
-            preOrder(node.right);
-        }
-    }
-    public static void postOrder(Node node) {
-        if(node!=null){
-            postOrder(node.left);
-            postOrder(node.right);
-            System.out.print(node.data);
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        tree = new Node[N + 1];
-        StringTokenizer st;
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(bf.readLine());
-            char rootValue = st.nextToken().charAt(0);
-            char leftValue = st.nextToken().charAt(0);
-            char rightValue = st.nextToken().charAt(0);
+    static boolean[] visit;
+    static int max = Integer.MIN_VALUE;
+    static int n, m;
+    static int count;
+    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
-            if (tree[rootValue - 'A'] == null) {
-                tree[rootValue - 'A'] = new Node(rootValue);
-            }
-            if (leftValue != '.') {
-                tree[leftValue - 'A'] = new Node(leftValue);
-                tree[rootValue - 'A'].left = tree[leftValue - 'A'];
-            }
-            if (rightValue != '.') {
-                tree[rightValue - 'A'] = new Node(rightValue);
-                tree[rootValue - 'A'].right = tree[rightValue - 'A'];
-            }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for(int i=0;i<=n;i++){
+            list.add(new ArrayList<>());
         }
-        preOrder(tree[0]);
-        System.out.println();
-        inOrder(tree[0]);
-        System.out.println();
-        postOrder(tree[0]);
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(b).add(a);
+        }
+        int[] result = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            visit = new boolean[n + 1];
+            count = 0;
+            bfs(i);
+            result[i] = count;
+            max = Math.max(count, max);
+
+        }
+        for (int i = 1; i <= n; i++) {
+            if (result[i] == max)
+                sb.append(i+" ");
+        }
+        System.out.println(sb);
+
+    }
+
+    public static void bfs(int x) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(x);
+        visit[x] = true;
+        while (!q.isEmpty()) {
+            int v = q.poll();
+            for(int i : list.get(v)){
+                if(!visit[i]){
+                    q.add(i);
+                    visit[i] = true;
+                    count++;
+                }
+            }
+
+        }
     }
 }
