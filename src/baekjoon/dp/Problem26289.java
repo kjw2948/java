3cdb500350b4d7305c7a47f5f76188afcec6b610
kjw2948 <kjw970313@cau.ac.kr>
2024-07-21 21:54:30 +0900

    [Update] support 추가

---
+++ src/baekjoon/dp/Problem26289.java
@@ -0,0 +1,20 @@
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem26289 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+
+    public void dp() {
+
+
+    }
+
+}
