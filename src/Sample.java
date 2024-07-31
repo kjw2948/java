774e3e3a14174186c03880caeccadf9b0d6299f1
kjw2948 <kjw970313@cau.ac.kr>
2024-07-31 20:49:17 +0900

    [Update] 예시 추가

---
+++ src/Sample.java
@@ -0,0 +1,27 @@
+public class Sample {
+    private String name;
+    private int age;
+    private String address;
+    private String phoneNumber;
+    private Test test;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Test getTest() {
+        return test;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    private String test1;
+    private String test2;
+
+    public String getTest1() {
+        return test1;
+    }
+
+    public String getTest2() {
+        return test2;
+    }
+}
