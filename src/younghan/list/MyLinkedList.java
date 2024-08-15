f33a3ccc914ff727495f60d7cf1f3c6eade8b657
kjw2948 <kjw970313@cau.ac.kr>
2024-08-15 21:32:47 +0900

    [Update] 연결리스트 구현하기 - 제네릭

--- src/younghan/list/MyLinkedList.java
+++ src/younghan/list/MyLinkedList.java
@@ -1,31 +1,31 @@
 package younghan.list;
 
-public class MyLinkedList {
+public class MyLinkedList<E> {
 
-    private Node first;
+    private Node<E> first;
     private int size = 0;
 
 
-    public void add(int index, Object e) {
-        Node newNode = new Node(e);
+    public void add(int index, E e) {
+        Node<E> newNode = new Node<>(e);
         if (index == 0) {
             newNode.next = first;
             first = newNode;
         } else {
-            Node prevNode = getNode(index - 1);
+            Node<E> prevNode = getNode(index - 1);
             newNode.next = prevNode.next;
             prevNode.next = newNode;
         }
         size++;
     }
 
-    public Object remove(int index) {
-        Node removeNode = getNode(index);
-        Object removeItem = removeNode.item;
+    public E remove(int index) {
+        Node<E> removeNode = getNode(index);
+        E removeItem = removeNode.item;
         if (index == 0) {
             first = removeNode.next;
         } else {
-            Node prev = getNode(index - 1);
+            Node<E> prev = getNode(index - 1);
             prev.next = removeNode.next;
         }
         removeNode.item = null;
@@ -34,37 +34,37 @@ public class MyLinkedList {
         return removeItem;
     }
 
-    private Node getLastNode() {
-        Node x = first;
+    private Node<E> getLastNode() {
+        Node<E> x = first;
         while (x.next != null) {
             x = x.next;
         }
         return x;
     }
 
-    public Object set(int index, Object element) {
-        Node x = getNode(index);
-        Object oldValue = x.item;
+    public E set(int index, E element) {
+        Node<E> x = getNode(index);
+        E oldValue = x.item;
         x.item = element;
         return oldValue;
     }
 
-    public Object get(int index) {
-        Node node = getNode(index);
+    public E get(int index) {
+        Node<E> node = getNode(index);
         return node.item;
     }
 
-    private Node getNode(int index) {
-        Node x = first;
+    private Node<E> getNode(int index) {
+        Node<E> x = first;
         for (int i = 0; i < index; i++) {
             x = x.next;
         }
         return x;
     }
 
-    public int indexOf(Object o) {
+    public int indexOf(E o) {
         int index = 0;
-        for (Node x = first; x != null; x=x.next) {
+        for (Node<E> x = first; x != null; x=x.next) {
             if (o.equals(x.item)) {
                 return index;
             }
@@ -86,4 +86,31 @@ public class MyLinkedList {
                 ", size=" + size +
                 '}';
     }
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+
+        public Node(E item) {
+            this.item = item;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            Node<E> x = this;
+            sb.append("[");
+            while (x != null) {
+                sb.append(x.item);
+                if (x.next != null) {
+                    sb.append("->");
+                }
+                x = x.next;
+            }
+            sb.append("]");
+
+            return sb.toString();
+        }
+    }
 }
+
+
