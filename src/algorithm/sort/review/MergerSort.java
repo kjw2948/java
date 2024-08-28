c10a0f1b7f4fd9ca4d6468cb4a6b447ffeb724e5
kjw2948 <kjw970313@cau.ac.kr>
2024-08-28 16:22:25 +0900

    [Update] 합병정렬

--- src/algorithm/sort/review/MergerSort.java
+++ src/algorithm/sort/review/MergerSort.java
@@ -4,21 +4,42 @@ import java.util.Arrays;
 
 public class MergerSort {
     static int[] arr = {4, 2, 3, 6, 8, 7, 5, 1};
-    static int[] result = new int[arr.length];
     public static void main(String[] args) {
-        mergeSort(arr, 0, arr.length);
+
     }
 
-    public static void merge(int[] arr, int left, int right) {
+    public static void mergeSort(int[] arr, int[] result, int left, int right) {
+        int[] tmp = new int[arr.length];
         int mid = (left + right) / 2;
-        if (right - left > 1) {
-            merge(Arrays.copyOfRange(arr, left, mid),left, mid);
-            merge(Arrays.copyOfRange(arr, mid, right),mid, right);
+        if (left < right) {
+            mergeSort(arr,tmp,left, mid);
+            mergeSort(arr,tmp,mid+1, right);
+            merge(arr, tmp,left, mid, right);
         }
-        mergeSort(arr, left, right);
     }
 
-    public static void mergeSort(int[] arr, int left, int right) {
+    public static void merge(int[] arr, int[] tmp, int left, int mid,  int right) {
+        for (int i = left; i <= right; i++) {
+            tmp[i] = arr[i];
+        }
+        int n1 = left;
+        int n2 = mid+1;
+        int index = left;
+
+        while (n1 <= mid && n2 <= right) {
+            if (tmp[n1] > tmp[n2]) {
+                arr[index] = tmp[n2];
+                n2++;
+            } else {
+                arr[index] = tmp[n1];
+                n1++;
+            }
+            index ++;
+        }
+
+        for (int i = 0; i <= mid - n1; i++) {
+            arr[index + i] = tmp[n1 + i];
+        }
 
     }
 }
