e8e58b2ac76d1f9ee4efed11f030e5069d44dfe6
kjw2948 <kjw970313@cau.ac.kr>
2024-08-29 19:46:31 +0900

    [Update] dP

--- src/nocode/dp/MinCostClimb.java
+++ src/nocode/dp/MinCostClimb.java
@@ -4,11 +4,19 @@ public class MinCostClimb {
     static int[] cost;
     static int[] memo;
     public static int climb(int n) {
-
+        if (n == 1 || n == 2) {
+            return memo[n] = cost[n-1];
+        }
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        return memo[n] = Math.min(climb(n-1) + cost[n-1], climb(n-2) + cost[n - 2]);
     }
     public static void main(String[] args) {
         int n = 3;
-        memo = new int[n];
+        memo = new int[n+1];
         cost = new int[]{10, 15, 20};
+        climb(n);
+        System.out.println(memo[n]);
     }
 }
