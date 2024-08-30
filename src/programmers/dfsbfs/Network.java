973980fabb0c946cd806a5a33f03ff640dbf2916
kjw2948 <kjw970313@cau.ac.kr>
2024-08-30 21:53:11 +0900

    [Update] bfs

---
+++ src/programmers/dfsbfs/Network.java
@@ -0,0 +1,19 @@
+package programmers.dfsbfs;
+
+import java.util.Arrays;
+
+public class Network {
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        System.out.println(solution(n, computers));
+
+
+    }
+
+}
