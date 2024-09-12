cdd3b28802420ae925ff1b0ed78d5cda355c2126
kjw2948 <kjw970313@cau.ac.kr>
2024-09-12 15:36:56 +0900

    [Update] Thread

---
+++ src/advancedjava/thread/control/ThreadStateMain.java
@@ -0,0 +1,32 @@
+package advancedjava.thread.control;
+
+import static advancedjava.util.MyLogger.log;
+
+public class ThreadStateMain {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new MyRunnable(), "myThread");
+        log("myThread.state1 = "+thread.getState());
+        log("myThread.start()");
+        thread.start();
+        Thread.sleep(500);
+        log("myThread.state3 : " + thread.getState());
+    }
+
+    static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            log("start");
+            log("myThread.state2 : " + Thread.currentThread().getState());
+            log("sleep() start");
+            try {
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            log("sleep() end");
+        }
+    }
+}
