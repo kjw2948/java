b1dd8978666b4a28d267823f2919fe54e1f0d718
kjw2948 <kjw970313@cau.ac.kr>
2024-10-08 19:40:07 +0900

    [Update] 백준 4963번

--- src/eyelevel/dfsbfs/Day2_4963.java
+++ src/eyelevel/dfsbfs/Day2_4963.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class Day2_4963 {
     static int[][] map;
     static boolean[][] visit;
-    static int[][] pos = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int[][] pos = {{0, 1}, {0, -1}, {1, 0}, {-1, 0},{1,1},{1,-1},{-1,1},{-1,-1}}; // 상하좌우 대각을 나타내는 배열
     static int island;
     static StringBuilder sb;
     public static void main(String[] args) throws IOException {
@@ -18,12 +18,12 @@ public class Day2_4963 {
 
         while (true) {
             island = 0;
+            // #1 Input 받기
             st = new StringTokenizer(br.readLine());
-
             int w = Integer.parseInt(st.nextToken());
             int h = Integer.parseInt(st.nextToken());
 
-            if(w==0 && h==0) break;
+            if(w==0 && h==0) break; // 종료조건
             map = new int[h][w];
             visit = new boolean[h][w];
 
@@ -34,33 +34,34 @@ public class Day2_4963 {
                 }
             }
 
-            bfs(0, 0);
+
+            // 2. 모든 위치를 순회하면서 조건에 맞으면 bfs 진행
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (map[i][j] == 1 && !visit[i][j]) {
+                        bfs(i, j);
+                        island++;
+                    }
+                }
+            }
+            sb.append(island).append("\n");
         }
         System.out.println(sb.toString());
     }
 
+    // 3.bfs 진행
     static void bfs(int h, int w) {
+        // 방문한곳은 체크
         visit[h][w] = true;
-        int count = 0;
-        if (map[h][w] == 0) {
-            return;
-        }
 
-
-        for (int i = 0; i < 4; i++) {
+        // 한 점에 대해서 상하좌우 대각선 확인하여 방문한적 없는 땅이면 다시 bfs 반복
+        for (int i = 0; i < pos.length; i++) {
             int dh = h + pos[i][0];
             int dw = w + pos[i][1];
 
             if (dh >= 0 && dw >= 0 && dh < visit.length && dw < visit[0].length && !visit[dh][dw] && map[dh][dw] == 1) {
                 bfs(dh, dw);
-                visit[dh][dw] = false;
-                count ++;
             }
         }
-        if (count == 0) {
-            island ++;
-            sb.append(island);
-        }
     }
-
 }
