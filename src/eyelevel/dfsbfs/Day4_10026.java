524f8b79e2dd9f57fe8129b118f39e2ad7b80562
kjw2948 <kjw970313@cau.ac.kr>
2024-10-10 23:38:42 +0900

    [Update] 백준 10026번

--- src/eyelevel/dfsbfs/Day4_10026.java
+++ src/eyelevel/dfsbfs/Day4_10026.java
@@ -6,30 +6,21 @@ import java.io.InputStreamReader;
 
 public class Day4_10026 {
     static boolean[][] visit;
-    static boolean[][] rgVisit;
     static char[][] map;
-    static char[][] rgMap;
     static int[][] pos = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    static int N, rgb, rg, region, rgRegion;
+    static int N, rgb, rg, region;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         visit = new boolean[N][N];
-        rgVisit = new boolean[N][N];
         map = new char[N][N];
-        rgMap = new char[N][N];
 
 
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
             for (int j = 0; j < s.length(); j++) {
-                char c = s.charAt(j);
-                map[i][j] = c;
-                rgMap[i][j] = c;
-                if (c == 'G') {
-                    rgMap[i][j] = 'R';
-                }
+                map[i][j] = s.charAt(j);
             }
         }
 
@@ -37,27 +28,44 @@ public class Day4_10026 {
             System.out.println(1+" "+1);
         }
 
+        // 정상
+        int result = 0;
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
                 if (!visit[i][j]) {
-                    rgb += (dfs(i, j) + 1) / region;
-                    region = 0;
+                    dfs(i, j);
+                    result++;
                 }
-                /*
-                if (!rgVisit[i][j]) {
-                    rg += (rgDfs(i, j) + 1) / rgRegion;
-                    rgRegion = 0;
-                }
-
-                 */
             }
         }
+        rgb = result;
+        result = 0;
+        visit = new boolean[N][N];
+
+        //비정상
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == 'G') {
+                    map[i][j] = 'R';
+                }
+            }
+        }
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (!visit[i][j]) {
+                    dfs(i, j);
+                    result++;
+                }
+            }
+        }
+
+        rg = result;
         System.out.println(rgb +" " +rg);
     }
 
-    static int dfs(int x, int y) {
+    static void dfs(int x, int y) {
         visit[x][y] = true;
-        region ++;
 
         for (int i = 0; i < pos.length; i++) {
             int dx = x + pos[i][0];
@@ -67,21 +75,5 @@ public class Day4_10026 {
                 dfs(dx, dy);
             }
         }
-        return region;
-    }
-
-    static int rgDfs(int x, int y) {
-        rgVisit[x][y] = true;
-        rgRegion ++;
-
-        for (int i = 0; i < pos.length; i++) {
-            int dx = x + pos[i][0];
-            int dy = y + pos[i][1];
-
-            if (dx >= 0 && dx < N && dy >= 0 && dy < N && !rgVisit[dx][dy] && rgMap[x][y] == rgMap[dx][dy]) {
-                rgDfs(dx, dy);
-            }
-        }
-        return rgRegion;
     }
 }
