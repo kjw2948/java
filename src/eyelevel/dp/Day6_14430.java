f940535e2b1e1a303752c87af17e8e492cb5c342
kjw2948 <kjw970313@cau.ac.kr>
2024-10-12 23:34:11 +0900

    [Update] 백준 14430

--- src/eyelevel/dp/Day6_14430.java
+++ src/eyelevel/dp/Day6_14430.java
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Day6_14430 {
-    static int N,M, mineral;
+    static int N, M;
     static int[][] map;
-    static int[][] pos = {{0, 1}, {1, 0}}; // 오른쪽 아래
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,64 +24,19 @@ public class Day6_14430 {
             }
         }
 
-        mineral = map[0][0] * 1000;
-        map[0][0] += 50;
         for (int i = 1; i < N; i++) {
-            if (map[i][0] == 0) {
-                map[i][0] = map[i - 1][0];
-            }
-            if (map[i][0] == 1) {
-                map[i][0] = map[i - 1][0] + 1000;
-            }
+            map[i][0] += map[i - 1][0];
         }
 
         for (int i = 1; i < M; i++) {
-            if (map[0][i] == 0) {
-                map[0][i] = map[0][i-1];
-            }
-            if (map[0][i] == 1) {
-                map[0][i] = map[0][i-1] + 1000;
-            }
+            map[0][i] += map[0][i - 1];
         }
-
 
         for (int i = 1; i < N; i++) {
             for (int j = 1; j < M; j++) {
-                if (map[i][j] % 10 != 5) {
-                    dp(i, j);
-                }
+                map[i][j] += Math.max(map[i - 1][j], map[i][j - 1]);
             }
         }
-
-        System.out.println(mineral / 1000);
-
-    }
-
-    static void dp(int x, int y) {
-        int max = Integer.max(map[x- 1][y], map[x][y - 1]);
-
-        if (map[x][y] == 1) map[x][y] = max + 1000;
-        if (map[x][y] == 0) map[x][y] = max;
-
-        mineral = Integer.max(map[x][y], mineral);
-
+        System.out.println(map[N - 1][M - 1]);
     }
 }
-
-
-/*
-3 4
-0 1 1 0
-1 1 0 0
-0 1 0 0
-   0 1000 2000 2000
-1000 2000 2000 2000
-1000 3000 2000 2000
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.printf("%4d ", map[i][j]);
-            }
-            System.out.println();
-        }
- */
