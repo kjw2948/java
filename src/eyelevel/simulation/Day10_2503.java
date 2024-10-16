d7fd6489faedd47b7acbb1c94e549d315fd62ffa
kjw2948 <kjw970313@cau.ac.kr>
2024-10-16 18:27:55 +0900

    [Update] 백준 13567번

---
+++ src/eyelevel/simulation/Day10_2503.java
@@ -0,0 +1,19 @@
+package eyelevel.simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Day10_2503 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+        }
+    }
+}
