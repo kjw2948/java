f237b2a2e8805c11ca868d149280101114bd75dc
kjw2948 <kjw970313@cau.ac.kr>
2024-10-16 23:20:16 +0900

    [Update] 백준 2503번

--- src/eyelevel/simulation/Day10_2503.java
+++ src/eyelevel/simulation/Day10_2503.java
@@ -9,11 +9,66 @@ public class Day10_2503 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            
+        int ans = 0;
+
+        int[] arr = new int[1000];
+        for (int i = 123; i <= 987; i++) {
+            String str = Integer.toString(i);
+
+            if (str.charAt(0) == str.charAt(1) || str.charAt(0) == str.charAt(2) || str.charAt(1) == str.charAt(2)
+                    ||str.charAt(0)=='0' || str.charAt(1)=='0' ||str.charAt(2)=='0') {
+                arr[i] = -1;
+            }
         }
+        String strNum;
+        int strike;
+        int ball;
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            strNum = st.nextToken();
+            strike = Integer.parseInt(st.nextToken());
+            ball = Integer.parseInt(st.nextToken());
+
+            int strikeCnt = 0;
+            int ballCnt = 0;
+
+            for (int idx = 123; idx <= 987; idx++) {
+                if (arr[idx] == -1) continue;
+                int passCnt = 0;
+                strikeCnt = 0;
+                String strIdx = Integer.toString(idx);
+                for (int j = 0; j < 3; j++) {
+                    if (strNum.charAt(j) == strIdx.charAt(j)) strikeCnt++;
+                }
+
+                ballCnt = 0;
+                for (int p = 0; p < 3; p++) {
+                    for (int q = 0; q < 3; q++) {
+                        if ((strNum.charAt(p) == strIdx.charAt(q)) && (p != q)) {
+                            ballCnt++;
+                        }
+                    }
+                }
+                if ((strike != strikeCnt) || ball != ballCnt) {
+                    arr[idx] = -1;
+                    continue;
+                }
+
+                //통과된 횟수 카운트
+                if ((strike == strikeCnt) && (ball == ballCnt)) {
+                    arr[idx]++;
+                }
+            }
+
+        }
+
+        for (int i = 123; i <= 987; i++) {
+
+            if (arr[i] != -1 && arr[i] == N) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
     }
 }
