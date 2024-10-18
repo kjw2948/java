f1a89997b9bda42f00eea0145f551b939cec328e
kjw2948 <kjw970313@cau.ac.kr>
2024-10-18 19:58:32 +0900

    [Update] 백준 13335번

---
+++ src/eyelevel/simulation/Day12_13335.java
@@ -0,0 +1,30 @@
+package eyelevel.simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Day12_13335 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+
+        int[]arr = new int[N+1];
+        Deque<Integer> queue = new LinkedList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+
+    }
+}
