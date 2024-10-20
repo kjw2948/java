d34af84449ee7af2cf6e8131c57a8370eb20a8fa
kjw2948 <kjw970313@cau.ac.kr>
2024-10-20 23:25:35 +0900

    [Update] 백준 11663

--- src/eyelevel/binarySearch/Day14_11663.java
+++ src/eyelevel/binarySearch/Day14_11663.java
@@ -3,73 +3,63 @@ package eyelevel.binarySearch;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Day14_11663 {
+    static int n, m;
+    static int [] arr;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        // Input 입력
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[] point = new int[N + 1];
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
-
-        // N개의 점 저장
-        for (int i = 1; i <= N ; i++) {
-            point[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < M; i++) {
-            int left = 1;
-            int right = N;
-            int sIdx = left;
-            int eIdx = right;
-
+        Arrays.sort(arr);
+        for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int lo = Integer.parseInt(st.nextToken());
+            int hi = Integer.parseInt(st.nextToken());
 
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                if (point[mid] == start || left == right) {
-                    sIdx = mid;
-                    break;
-                } else if (point[mid] < start) {
-                    left = mid + 1;
-                    sIdx = mid;
-                } else {
-                    right = mid;
-                }
-            }
-            System.out.println("sIdx = " + sIdx);
-            left = sIdx;
-            right = N;
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                if (point[mid] == end) {
-                    eIdx = mid;
-                    break;
-                } else if (point[mid] < end) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                    eIdx = mid - 1;
-                }
-                if (left == right) {
-                    break;
-                }
-            }
-            System.out.println("eIdx = " + eIdx);;
+            sb.append(upperbound(hi) - lowerbound(lo)).append("\n");
+        }
+        System.out.println(sb);
+    }
 
-            if ( sIdx>eIdx  || (sIdx == eIdx && point[eIdx] > end)) {
-                System.out.println(0);
-            }else {
-                System.out.println(eIdx - sIdx + 1);
+    public static int lowerbound(int key) {
+        int lo = 0;
+        int hi = n;
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key <= arr[mid]) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
             }
         }
+        return lo;
+    }
 
+    public static int upperbound(int key) {
+        int lo = 0;
+        int hi = n;
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < arr[mid]) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
     }
 }
