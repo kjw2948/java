ce4868396fb4c212214cd82a30d026a4bb6862e5
kjw2948 <kjw970313@cau.ac.kr>
2024-10-20 17:44:52 +0900

    [Update] 백준 17266번

--- src/eyelevel/binarySearch/Day13_17266.java
+++ src/eyelevel/binarySearch/Day13_17266.java
@@ -13,18 +13,22 @@ public class Day13_17266 {
         int M = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
-        int[] light = new int[M+1]; // 가로등 위치가 저장된
+        int[] light = new int[M+1]; // 가로등 위치가 저장된 배열
         for (int i = 1; i <= M; i++) {
             light[i] = Integer.parseInt(st.nextToken());
         }
+
+        // 맨앞 , 맨뒤중에서 크기가 더 큰 값을 check에 저장
         int check = Math.max(light[1], N - light[M]);
         if (M == 1) {
             System.out.println(check);
             return;
         }
+
         int height = 0;
         for (int i = 1; i <= M; i++) {
-            height = Math.max((light[i] - light[i - 1] + 1) / 2, height);
+            int num = (light[i] - light[i - 1] + 1) / 2;
+            height = Math.max(num, height);
         }
 
         if (height <= check) {
