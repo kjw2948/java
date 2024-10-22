bb2e5c50bd16477b0ab3bbb719fb384408708faf
kjw2948 <kjw970313@cau.ac.kr>
2024-10-22 22:05:09 +0900

    [Update] 백준 2805번

--- src/eyelevel/binarySearch/Day15_2805.java
+++ src/eyelevel/binarySearch/Day15_2805.java
@@ -11,43 +11,37 @@ public class Day15_2805 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int check = 0;
+        int result = 0;
 
         int[] tree = new int[N + 1];
-        int min = 0;
-        int max = Integer.MIN_VALUE;
+        int left = 0;
+        int right = Integer.MIN_VALUE;
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             tree[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, tree[i]);
-        }
-        if (min == max) {
-            if (M % N == 0) {
-                System.out.println(max - M / N);
-            } else {
-                System.out.println(max - M/N - 1);
-            }
-            return;
+            right = Math.max(right, tree[i]);
         }
 
-        while (min < max) {
-            int mid = min + (max - min) / 2;
-            long result = 0;
+        while (left < right) {
+            int mid = left + (right - left) / 2; // 이분탐색을 위한 중간값
+            long height = 0; // 자른 나무의 길이
             for (int i = 1; i <= N; i++) {
                 if (tree[i] >= mid) {
-                    result += tree[i] - mid;
+                    //N개의 나무를 탐색하면서 자르려는 높이 이하면 차이를 result 누적해서 더해준다
+                    height += tree[i] - mid;
                 }
             }
-            if (result == M) {
-                check = mid;
+            if (height == M) {
+                // 나무길이 합이 M과 같으면
+                result = mid;
                 break;
-            } else if (result < M) {
-                max = mid;
+            } else if (height < M) {
+                right = mid;
             } else {
-                check = mid;
-                min = mid+1;
+                result = mid;
+                left = mid+1;
             }
         }
-        System.out.println(check);
+        System.out.println(result);
     }
 }
