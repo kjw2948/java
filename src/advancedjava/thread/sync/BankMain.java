2983c5c7840d99974dde39b69e7bdd41a88eb144
kjw2948 <kjw970313@cau.ac.kr>
2024-10-29 16:38:53 +0900

    출금 예제

---
+++ src/advancedjava/thread/sync/BankMain.java
@@ -0,0 +1,21 @@
+package advancedjava.thread.sync;
+
+import static advancedjava.util.MyLogger.log;
+import static advancedjava.util.ThreadUtils.sleep;
+
+public class BankMain {
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount account = new BankAccountV1(1000);
+
+        Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
+        Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
+        t1.start();
+        t2.start();
+
+        sleep(1000);
+
+        t1.join();
+        t2.join();
+        log("최종 잔액 : " + account.getBalance());
+    }
+}
