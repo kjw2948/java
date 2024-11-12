aeccfc6a3a3ccdb041a80ba61c4af742febad187
kjw2948 <kjw970313@cau.ac.kr>
2024-11-12 17:31:19 +0900

    삼성 백준

--- src/eyelevel/dfsbfs/Day1_1326.java
+++ src/eyelevel/dfsbfs/Day1_1326.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Day1_1326 {
-    static boolean[] visit;
+    static int[] visit;
     static int[] arr;
     static int a, b, N;
 
@@ -17,18 +18,19 @@ public class Day1_1326 {
 
         N = Integer.parseInt(br.readLine());
         arr = new int[N + 1];
-        visit = new boolean[N + 1];
+        visit = new int[N + 1];
 
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            visit[i] = -1;
         }
 
         st = new StringTokenizer(br.readLine());
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
-
+        visit[a] ++;
 
         if (a == b) {
             System.out.println(0);
@@ -36,9 +38,65 @@ public class Day1_1326 {
             System.out.println(bfs());
         }
 
-
     }
 
+    static int bfs() {
+        Queue<int[]> queue = new ArrayDeque<>();  // (인덱스, 반복해서 저장되는 값)
+        queue.add(new int[]{a, visit[a]}); // 현재위치, 이동횟수
+        int next = 0;
+
+
+        while (!queue.isEmpty()) {
+            int[] nums = queue.poll();
+            int cur = nums[0];
+            int move = nums[1];
+
+            // 역방향
+            next = cur - arr[cur];
+            while (true) {
+                if (next < 0 || arr[next] != -1) {
+                    break;
+                }
+                if (visit[next] == -1) {
+                    visit[next] = move + 1;
+                    queue.add(new int[]{next, visit[next]});
+                    next -= arr[cur];
+                }
+                if (next == b) {
+                    return visit[next];
+                }
+            }
+            // 순방향
+            next = cur + arr[cur];
+            while (true) {
+                if (next > N || arr[next] != -1) {
+                    break;
+                }
+
+                if (visit[next] == -1) {
+                    visit[next] = move + 1;
+                    queue.add(new int[]{next, visit[next]});
+                    next += arr[cur];
+                }
+                if (next == b) {
+                    return visit[next];
+                }
+            }
+        }
+
+
+        return visit[b];
+    }
+
+
+
+
+
+
+
+
+
+    /*
     private static int bfs() {
         Queue<int[]> q = new ArrayDeque<>();
         q.add(new int[]{a, 0});
@@ -83,6 +141,8 @@ public class Day1_1326 {
         return -1;
 
     }
+
+     */
 }
     /*
     static boolean[] visit;
