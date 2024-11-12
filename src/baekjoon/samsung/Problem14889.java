cb00e0409c023fc4f26be5ad485c41d978da20c0
kjw2948 <kjw970313@cau.ac.kr>
2024-11-12 16:44:01 +0900

    삼성 백준

--- src/baekjoon/samsung/Problem14889.java
+++ src/baekjoon/samsung/Problem14889.java
@@ -2,6 +2,13 @@ package baekjoon.samsung;
 
 public class Problem14889 {
     public static void main(String[] args) {
+        // Input 받기
 
+
+        // (가고자하는 위치 - 현재위치) % 현재값 == 0 이면 +1 하고 종료
+
+        // 아니라면 값만큼 이동시킨 뒤 반복
+
+        // 최단경로니까 BFS!
     }
 }
