793b94445290c28df985febe457bb35ac4d9f3dd
kjw2948 <kjw970313@cau.ac.kr>
2024-11-26 21:28:59 +0900

    삼성 백준

---
+++ src/baekjoon/dp/Problem9465.java
@@ -0,0 +1,11 @@
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem9465 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
