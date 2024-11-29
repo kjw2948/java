446678431d4e2cfdf04eb725f4a50a8bc5df29c5
kjw2948 <kjw970313@cau.ac.kr>
2024-11-29 23:02:44 +0900

    삼성 백준

--- src/baekjoon/dp/Problem9465.java
+++ src/baekjoon/dp/Problem9465.java
@@ -1,11 +1,12 @@
 package baekjoon.dp;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Problem9465 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        int N = Integer.parseInt(br.readLine());
     }
 }
