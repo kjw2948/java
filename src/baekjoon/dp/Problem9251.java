27addf2dd87fbd96cb14c569713a79db166992a7
kjw2948 <kjw970313@cau.ac.kr>
2024-12-15 20:23:11 +0900

    삼성 백준

---
+++ src/baekjoon/dp/Problem9251.java
@@ -0,0 +1,20 @@
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+LCS
+ */
+public class Problem9251 {
+    static String[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+
+    }
+}
